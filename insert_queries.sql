e64eb1ff1c85adc043abf28c49bbe2e461b8966e
Simrandeep1 <simrandeepmann934@gmail.com>
2026-05-04 00:28:40 -0400

    Add files via upload

---
+++ insert_queries.sql
@@ -0,0 +1,26 @@
+
+SELECT * FROM members;
+SELECT * FROM branch;
+SELECT * FROM employees;
+SELECT * FROM issued_status;
+SELECT * FROM books;
+SELECT * FROM return_status;
+
+INSERT INTO issued_status
+(issued_id, issued_member_id, issued_book_name, issued_date, issued_book_isbn, issued_emp_id)
+VALUES
+('IS151', 'C118', 'The Catcher in the Rye', DATEADD(DAY, -24, GETDATE()), '978-0-553-29698-2', 'E108'),
+('IS152', 'C119', 'The Catcher in the Rye', DATEADD(DAY, -13, GETDATE()), '978-0-553-29698-2', 'E109'),
+('IS153', 'C106', 'Pride and Prejudice', DATEADD(DAY, -7, GETDATE()), '978-0-14-143951-8', 'E107'),
+('IS154', 'C105', 'The Road', DATEADD(DAY, -32, GETDATE()), '978-0-375-50167-0', 'E101');
+SELECT * FROM issued_status;
+-- Adding new column in return_status
+
+ALTER TABLE return_status
+ADD  book_quality VARCHAR(15) DEFAULT('Good');
+
+UPDATE return_status
+SET book_quality = 'Damaged'
+WHERE issued_id 
+    IN ('IS112', 'IS117', 'IS118');
+SELECT * FROM return_status;
